1a7f1d9eba768a987e0bb2c6d4a632bd8f149477
jhonatan-deev <jhonatan-deev@gmail.com>
2026-05-18 22:37:26 -0300

    chore: initialize Spring Boot project and add JPA

---
+++ src/main/java/medical/clinic/api/controller/HealthController.java
@@ -0,0 +1,12 @@
+package medical.clinic.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/hello")
+    public String health() {
+        return "Hello World!";
+    }
+}
