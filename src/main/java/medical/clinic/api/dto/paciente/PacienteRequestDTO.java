ccaaf4ad4c058fde776d9902f82fca161c906e50
jhonatan-deev <jhonatan-deev@gmail.com>
2026-05-25 12:47:31 -0300

    feat: cria entidade e Request DTO de paciente

---
+++ src/main/java/medical/clinic/api/dto/paciente/PacienteRequestDTO.java
@@ -0,0 +1,26 @@
+package medical.clinic.api.dto.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import medical.clinic.api.dto.endereco.EnderecoDTO;
+
+public record PacienteRequestDTO(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
+        String cpf,
+        @Valid
+        @NotNull
+        EnderecoDTO endereco
+) {
+}
